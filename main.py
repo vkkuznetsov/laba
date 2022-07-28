0deca7680d76b858ca1e0e3abf1fee734c77592d
VICTOR <vita.201581@yandex.ru>
2022-07-28 16:27:13 +0500

    test commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# from classes import *
+# x = 1
+# while x != 0:
+#     print(
+#         "Что хотите сделать\n 1 - Показать информацию о всех объектах\n 2 - Удалить объект\n 3 - Добавить объект")
+#     z = int(input())
+#     if z == 1:
+#         bmw = Car("BMW AMG", 2020, 700)
+#
+#         bmw.display_info()
+#
+#         airbus = Airplane("Boeing737", 1999, "AmericanAirlines")
+#
+#         airbus.display_info()
+#     elif z == 2:
+#         print("Какой объект хотите удалить?")
+#         z1 = int(input())
+#         if z1 == 1:
+#             airbus = Airplane("Boeing737", 1999, "AmericanAirlines")
+#
+#             airbus.display_info()
+#         elif z == 2:
+#             bmw = Car("BMW AMG", 2020, 700)
+#
+#             bmw.display_info()
+#     elif z == 3:
+#         shkoda = Car("Shkoda Octavia", 2019, 300)
+#         shkoda.display_info()
+#
+#
+
+
+
+#
+# people = [Person("Tom", 23), Student("Bob", 19, "Harvard"), Employy("Sam", 35, "Google")]
+# for person in people:
+#     if isinstance(person, Student):
+#         print(person.university)
+#     elif isinstance(person, Employy):
+#         print(person.company)
+#     else:
+#         print(person.name)
