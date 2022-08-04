73b422918a3b5e1b646bd8f276595880763d7c7a
VICTOR <vita.201581@yandex.ru>
2022-08-04 15:57:13 +0500

    жесткий коммит

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-print("Hello wordl")
-z = (1, 2, 3, 4)
-print(z)
+import numpy as np
+
+M = int(input())
+A = []
+for i in range(M):
+    A.append(list(map(int, input().split())))
+
+arr_matrix = np.array(A)
+
+for i in range(M//2):
+    arr_time = np.array(arr_matrix[i])
+    arr_matrix[i] = arr_matrix[M - 1 - i]
+    arr_matrix[M - 1 - i] = arr_time
+
+arr_trs = arr_matrix.transpose()
+
+print(arr_trs)
