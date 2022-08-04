cd4746f9039c28d6ad4b54ef0b7a2ac8db72c7df
VICTOR <vita.201581@yandex.ru>
2022-08-04 18:26:12 +0500

    done

--- test.py
+++ test.py
@@ -1,17 +1,8 @@
-import numpy as np
-
 M = int(input())
-A = []
+A = [list(map(int, input().split())) for i in range(M)]
+for i in range(M // 2):
+    A[i], A[M - 1 - i] = A[M - 1 - i], A[i]
 for i in range(M):
-    A.append(list(map(int, input().split())))
-
-arr_matrix = np.array(A)
-
-for i in range(M//2):
-    arr_time = np.array(arr_matrix[i])
-    arr_matrix[i] = arr_matrix[M - 1 - i]
-    arr_matrix[M - 1 - i] = arr_time
-
-arr_trs = arr_matrix.transpose()
-
-print(arr_trs)
+    print()
+    for j in range(M):
+        print(A[j][i], end=" ")
